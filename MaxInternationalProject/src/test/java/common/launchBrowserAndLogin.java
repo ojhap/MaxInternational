0b7ced778fc5db9970a2533727a8625613a42174
ojhap <pojha0507@gmail.com>
2022-05-15 21:08:06 -0700

    trying to update and push

--- MaxInternationalProject/src/test/java/common/launchBrowserAndLogin.java
+++ MaxInternationalProject/src/test/java/common/launchBrowserAndLogin.java
@@ -2,9 +2,10 @@ package common;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class launchBrowserAndLogin {
-	
+	@Test
 	public void setData(){
 			System.setProperty("webdriver.chrome.driver","C:\\selenium component\\chromedriver.exe");
 			 WebDriver driver = new ChromeDriver();
